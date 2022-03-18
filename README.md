d4c23e91ab83879bdb32490bf6536119c55f219d
Bi Wu <alerk_star@outlook.com>
2022-03-19 01:25:58 +0800

    deploy script;

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+```bash
+pip install py-solc-x
+solcx.install_solc('0.6.0')
+```
--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,15 +1,27 @@
-from brownie import accounts, config
+from brownie import accounts, config, SimpleStorage
 # import os
 
 
 def deploy_simple_storage():
-  # account = accounts[0]
+  account = accounts[0]
   # print(account)
   # account = accounts.load('my-account')
   # account =  accounts.add(os.getenv("PRIVATE_KEY"))
-  account =  accounts.add(config["wallets"]["from_key"])
-  print(account)
-  pass
+  # account =  accounts.add(config["wallets"]["from_key"])
+  # print(account)
+
+  simple_storage = SimpleStorage.deploy({ "from": account })
+  print(simple_storage)
+  # Transact
+  # Call
+  stored_value = simple_storage.retrieve()
+  print(stored_value)
+  transaction = simple_storage.store(15, {"from": account})
+  transaction.wait(1)
+  updated_stored_value = simple_storage.retrieve()
+  print(updated_stored_value)
+
+
 
 def main():
   deploy_simple_storage()
